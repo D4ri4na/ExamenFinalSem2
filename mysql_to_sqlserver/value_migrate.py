8f7cc95ad3ba8b83042819469e45ad7c21a92a01
nicole <lozadaleonn@gmail.com>
2024-12-13 17:51:04 -0400

    codigo de migracion comentado

--- mysql_to_sqlserver/value_migrate.py
+++ mysql_to_sqlserver/value_migrate.py
@@ -1,15 +1,8 @@
 from mysql_connection import MySQLConnection
 from mssql_connection import MSSQLConnection
-from dataclasses import dataclass
 from config import MYSQL_CONFIG
 
-@dataclass
-class Table:
-    name: str
-    columns: list
-    data: list
-
-# Mapping of MySQL data types to SQL Server data types
+# Map de tipos de datos de MySQL a tipos de datos de SQL Server
 data_type_mapping = {
     'int': 'INT',
     'tinyint': 'TINYINT',
@@ -53,50 +46,35 @@ def migrate_data():
         mssql_conn.connect()
 
         db_name = MYSQL_CONFIG['database']
-
-        # Check if the database already exists
-        check_db_query = f"SELECT * FROM sys.databases WHERE name = '{db_name}';"
-        db_exists = mssql_conn.execute_query(check_db_query)
-
-        if not db_exists:
-            create_db_query = f"CREATE DATABASE {db_name};"
-            mssql_conn.execute_update(create_db_query)
-            print(f"Database '{db_name}' created successfully.")
-        else:
-            print(f"Database '{db_name}' already exists.")
-
-        # Change context to the newly created database
         use_db_query = f"USE {db_name};"
         mssql_conn.execute_update(use_db_query)
 
-        # Get table names from MySQL
+        # Obtener nombres de tablas de MySQL
         tables = mysql_conn.execute_query("SHOW TABLES;")
-        print(f"Tables retrieved from MySQL: {tables}")  # Debug print statement
+        # print(f"Tablas recuperadas de MySQL: {tables}")  # verificaion 
 
         foreign_keys_list = []
 
-        # Start transaction
         mssql_conn.execute_update("BEGIN TRANSACTION;")
 
         try:
             for table in tables:
                 table_name = table['Tables_in_' + db_name]
-                print(f'Migrating table: {table_name}')
+                # print(f'Migrando tabla: {table_name}')  # verificaion 
 
-                # Check if the table exists in MS SQL Server
+                # Verificar si la tabla existe en MS SQL Server
                 table_exists_query = f"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{table_name}';"
                 table_exists = mssql_conn.execute_query(table_exists_query)
 
                 if not table_exists:
-                    # Get table schema from MySQL
                     schema_query = f"SHOW COLUMNS FROM {table_name};"
                     columns = mysql_conn.execute_query(schema_query)
 
-                    # Get primary key information
+                    # Obtener información de clave primaria
                     primary_key_query = f"SHOW KEYS FROM {table_name} WHERE Key_name = 'PRIMARY';"
                     primary_keys = mysql_conn.execute_query(primary_key_query)
 
-                    # Get foreign key information
+                    # Obtener información de clave foránea
                     foreign_key_query = f"""
                         SELECT
                             kcu.constraint_name,
@@ -115,29 +93,27 @@ def migrate_data():
                     """
                     foreign_keys = mysql_conn.execute_query(foreign_key_query)
 
-                    # Check if the table has foreign keys
                     if foreign_keys:
                         foreign_keys_list.extend(foreign_keys)
 
-                    # Create table in MS SQL Server with corrected data types
+                    # Crear tabla en MS SQL Server con tipos de datos corregidos
                     create_table_query = f"CREATE TABLE {table_name} ("
                     for column in columns:
                         col_name = column['Field']
                         col_type = column['Type']
 
-                        # Map MySQL data type to SQL Server data type
+                        # Mapear tipo de datos de MySQL a tipo de datos de SQL Server
                         col_type_parts = col_type.split('(')
                         base_type = col_type_parts[0]
                         mapped_type = data_type_mapping.get(base_type, 'VARCHAR')
 
                         if len(col_type_parts) > 1:
-                            # Handle types with length specification
+                            # Manejar tipos con especificación de longitud
                             length = col_type_parts[1].replace(')', '')
                             mapped_type += f"({length})"
 
                         create_table_query += f"{col_name} {mapped_type}, "
 
-                    # Add primary key constraint
                     if primary_keys:
                         primary_key_columns = [key['Column_name'] for key in primary_keys]
                         primary_key_constraint = f"PRIMARY KEY ({', '.join(primary_key_columns)})"
@@ -146,51 +122,44 @@ def migrate_data():
                     create_table_query = create_table_query.rstrip(', ') + ");"
                     mssql_conn.execute_update(create_table_query)
 
-
-
         except Exception as e:
-            # Rollback transaction in case of error
             mssql_conn.execute_update("ROLLBACK;")
-            print(f"Error during table creation or foreign key addition: {e}")
+            print(f"Error durante la creación de la tabla o la adición de claves foráneas: {e}")
             return
-        
-        
 
-
-        # Insert data into the tables
+        # Insertar datos en las tablas
         for table in tables:
             table_name = table['Tables_in_' + db_name]
-            print(f'Inserting data into table: {table_name}')
+            # print(f'Insertando datos en la tabla: {table_name}') verificacion
 
-            # Get data from MySQL
             data_query = f"SELECT * FROM {table_name};"
             data = mysql_conn.execute_query(data_query)
 
-            # Insert data into MS SQL Server
+            # Insertar datos en MS SQL Server
             for row in data:
                 insert_query = f"INSERT INTO {table_name} VALUES ("
                 for value in row.values():
                     if isinstance(value, str):
-                        insert_query += f"'{value.replace("'", "''")}', "  # Escape single quotes
+                        insert_query += f"'{value.replace("'", "''")}', "  # Escapar comillas simples
                     else:
                         insert_query += f"'{value}', "
                 insert_query = insert_query.rstrip(', ') + ");"
                 try:
                     mssql_conn.execute_update(insert_query)
                 except Exception as e:
-                    print(f'Error: {e}')  # Show the error but continue with the next record
+                    print(f'Error: {e}') 
 
-         # Add foreign key constraints to MS SQL Server
+        # Agregar restricciones de clave foránea a MS SQL Server
         for fk in foreign_keys_list:
-            # Imprimir toda la información de la clave foránea
-            #print(f"Foreign Key Info: {fk}")
+           
+            #print(f"Información de Clave Foránea: {fk}") verificacion
 
             constraint_name = fk['CONSTRAINT_NAME']
             table_name = fk['TABLE_NAME']
             column_name = fk['COLUMN_NAME']
             referenced_table_name = fk['REFERENCED_TABLE_NAME']
             referenced_column_name = fk['REFERENCED_COLUMN_NAME']
-            
+
             alter_table_query = f"""
             ALTER TABLE {table_name}
             ADD CONSTRAINT {constraint_name}
@@ -200,16 +169,15 @@ def migrate_data():
 
             try:
                 mssql_conn.execute_update(alter_table_query)
-                print(f"Foreign key constraint added successfully to table '{table_name}'.")
+                # print(f"Restricción de clave foránea agregada exitosamente a la tabla '{table_name}'.") verificacion
             except Exception as e:
-                print(f"Error adding foreign key constraint to table '{table_name}': {e}")
+                print(f"Error al agregar restricción de clave foránea a la tabla '{table_name}': {e}")
 
-        # Add "modificación" column to each existing table
+        # Agregar columna "modificación" a cada tabla existente
         for table in tables:
             table_name = table['Tables_in_' + db_name]
-            print(f'Adding column "modificación" to table: {table_name}')
+            # print(f'Agregando columna "modificación" a la tabla: {table_name}') verificacion
 
-            # Check if the column already exists
             check_column_query = f"""
                 SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
                 WHERE TABLE_NAME = '{table_name}' AND COLUMN_NAME = 'modificacion';
@@ -220,12 +188,12 @@ def migrate_data():
                 alter_table_query = f"ALTER TABLE {table_name} ADD modificacion DATETIME;"
                 try:
                     mssql_conn.execute_update(alter_table_query)
-                    print(f'Column "modificación" added to table: {table_name}')
+                    # print(f'Columna "modificación" agregada a la tabla: {table_name}') verificacion
                 except Exception as e:
-                    print(f'Error adding column "modificación" to table {table_name}: {e}')
+                    print(f'Error al agregar columna "modificación" a la tabla {table_name}: {e}')
 
     except Exception as e:
-        print(f'Error during migration: {e}')
+        print(f'Error durante la migración: {e}')
     finally:
         mysql_conn.close()
         mssql_conn.close()
